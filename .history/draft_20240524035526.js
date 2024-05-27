48bda80e85c7df9959f9b30ff4b5f1431002d9a3
Svetoslav <veiv@mail.ru>
2024-05-27 08:34:22 +0300

    27 may

---
+++ .history/draft_20240524035526.js
@@ -0,0 +1,9 @@
+// const matrix = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+// ];
+
+// // - - - - - 
+https://codesandbox.io/p/sandbox/function-t-3-w4kcfh
+
---
+++ .history/draft_20240524040111.js
@@ -0,0 +1,20 @@
+// const matrix = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+// ];
+
+// // - - - - - 
+function getDivisorsCount(n) {
+    let divisors = [];
+    for (let i = 1; i <= n; i++) {
+        if (n % i === 0) {
+            divisors.push(i);
+        } else if (((Number.isInteger(n)) || n !== Number)) {
+            console.log(`${n} должен быть целым числом и больше нуля!`);
+            return Nan;
+        }
+    }
+}
+console.log(getDivisorsCount(4));
+
